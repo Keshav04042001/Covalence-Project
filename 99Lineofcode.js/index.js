39abad1b1ab6f15900ee2f34a7053dd81517a7b7
Keshav Agrawal <56186828+Keshav04042001@users.noreply.github.com>
2021-01-30 23:46:51 +0530

    Add files via upload

---
+++ 99Lineofcode.js/index.js
@@ -0,0 +1,7 @@
+var frineds = ['keshav','bhupat','shri','vikas','banwari'];
+for(var i =0;i<frineds.length;i++)
+{
+    for(var j =99;j>0;j--){
+    console.log(j + 'lines of code in the file, ' + frineds[i] + 'strikes one out, clears it all out')
+    }
+}
